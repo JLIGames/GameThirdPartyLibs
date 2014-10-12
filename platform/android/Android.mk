92171f8e819aeb3942ba6a8e8e0c80dff02f95ce
james_folk <jamesfolk1@gmail.com>
2014-10-12 03:00:10 -0400

    added the logging functions for android

--- platform/android/Android.mk
+++ platform/android/Android.mk
@@ -7,5 +7,7 @@ LOCAL_SRC_FILES := File.cpp \
                    Input.cpp \
                    Log.cpp \
                    Sound.cpp
+                   
+LOCAL_LDLIBS    := -llog
 
 include $(BUILD_STATIC_LIBRARY)
---
+++ platform/android/Log.cpp
@@ -0,0 +1,22 @@
+#include<android/log.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+//#define LOGD(LOG_TAG, ...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+//#define LOGV(LOG_TAG, ...) __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, __VA_ARGS__)
+//#define LOGE(LOG_TAG, ...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+//#define LOGI(LOG_TAG, ...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+
+void Log(char *format, ...)
+{
+	va_list args;
+
+	va_start(args, format);
+
+	__android_log_print(ANDROID_LOG_INFO, "JLIGAMEENGINE", format, args);
+
+	vprintf(format, args);
+	va_end(args);
+
+
+}
