f06a50033f33e0052f10abf9fbee23df858027cb
james_folk <jamesfolk1@gmail.com>
2014-10-14 01:55:36 -0400

    Added an error log call. Added a base opengl calls.

--- platform/android/Log.cpp
+++ platform/android/Log.cpp
@@ -3,11 +3,6 @@
 #include <stdarg.h>
 #include "Log.h"
 
-//#define LOGD(LOG_TAG, ...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
-//#define LOGV(LOG_TAG, ...) __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, __VA_ARGS__)
-//#define LOGE(LOG_TAG, ...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
-//#define LOGI(LOG_TAG, ...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
-
 void Log(const char *format, ...)
 {
 	va_list args;
@@ -18,6 +13,16 @@ void Log(const char *format, ...)
 
 	vprintf(format, args);
 	va_end(args);
+}
 
+void LogError(const char *format, ...)
+{
+	va_list args;
 
+	va_start(args, format);
+
+	__android_log_print(ANDROID_LOG_ERROR, "JLIGAMEENGINE", format, args);
+
+	vprintf(format, args);
+	va_end(args);
 }
--- platform/core/Log.h
+++ platform/core/Log.h
@@ -1,1 +1,2 @@
 void Log(const char *format, ...);
+void LogError(const char *format, ...);
